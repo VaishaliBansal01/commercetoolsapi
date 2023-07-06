03e3410b02980735d116140d6d5223db3a829e83
VaishaliBansal01 <119921403+VaishaliBansal01@users.noreply.github.com>
2023-07-06 09:20:38 +0530

    Update CTConfiguration.java
    just review code

--- src/main/java/com/demo/CT/config/CTConfiguration.java
+++ src/main/java/com/demo/CT/config/CTConfiguration.java
@@ -9,19 +9,17 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class CTConfiguration {
-//   static String client_id = "6Y8G5HQZUZWmjUdjRB9fT7rg";
-//   static String client_secret = "C6f4jmhJ3G3CWd8aUly9w010rzlRObBL";
-//    static String projectKey = "new-vaishali-ct";
+
 
     @Bean
     public static ProjectApiRoot createApiClient() {
         final ProjectApiRoot apiRoot = ApiRootBuilder.of()
                 .defaultClient(ClientCredentials.of()
-                                .withClientId("t_bO15QSdTZXoqnuQkpq5ei6")
-                                .withClientSecret("hL8kpGc7SH-LYbFVxytif4r_zGdG1_0r")
+                                .withClientId("client_id")
+                                .withClientSecret("client_secret")
                                 .build(),
                        ServiceRegion.GCP_AUSTRALIA_SOUTHEAST1)
-                .build("myntra-copy");
+                .build("project-key");
 
         return apiRoot;
     }
